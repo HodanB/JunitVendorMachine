3e61fd7a63e4ae18d88fc8ca7a3e8c4fd58f9a73
HodanB <Hodanbarkhad@outlook.com>
2022-06-28 22:09:08 +0100

    completed wallet testing

--- src/test/java/vendormachine/users/util/WalletTEST.java
+++ src/test/java/vendormachine/users/util/WalletTEST.java
@@ -1,11 +1,71 @@
 package vendormachine.users.util;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
+
 public class WalletTEST {
+	
+	
+	@Test
+    public void testWalletStartCred() {
+    	Wallet wallet = new Wallet(1.5f);
+    	assertEquals(1.5f,wallet.getAllCredit());
+    }
+    
+    @Test
+    public void testWalletStartCredBrand() {
+    	Wallet wallet = new Wallet("newBrand", 1.5f);
+    	assertEquals("newBrand", wallet.getBrand("gucci"));
+    	
+    }
 
     @Test
-    public void test1(){
+    public void testAddCredit(){
+    	Wallet wallet = new Wallet();
+    	wallet.addCredit(2f);
+    	assertEquals(1.5f,1.5f, 1f);
+    }
 
+    @Test
+    public void testGetCreditStored() {
+    	Wallet wallet = new Wallet();
+    	assertEquals(wallet.getCredit(0.5f), 0.5f);
+    }
+    
+    @Test
+    public void testGetCreditExceed() {
+    	Wallet wallet1 = new Wallet();
+    	assertEquals(wallet1.getCredit(1.5f), 0.0f);
+    }
+    
+    @Test
+    public void testGetAllCredit() {
+    	Wallet wallet1 = new Wallet();
+    	wallet1.addCredit(2f);
+    	wallet1.addCredit(3f);
+    	assertEquals(5.5f,wallet1.getAllCredit());
+    }
+    
+    @Test
+    public void testGetBrand() {
+    	Wallet wallet1 = new Wallet();
+    	assertEquals("Generic",wallet1.getBrand("gucci"));
+    }
+    
+    @Test
+    public void testSetCredit() {
+    	Wallet wallet1 = new Wallet();
+    	wallet1.setCredit(1.0f);
+    	assertEquals(1.0f,wallet1.getAllCredit());    	
+    }
+    
+    @Test
+    public void testSetBrand() {
+    	Wallet wallet1 = new Wallet();
+    	wallet1.setBrand("newBrand");
+    	assertEquals("newBrand",wallet1.getBrand("gucci"));  	
     }
 }
+
